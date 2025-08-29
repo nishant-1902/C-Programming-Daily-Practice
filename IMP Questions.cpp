f0c380c3cac0f92cd7cf26503e13126095feba7b
Nishant Jadhav <159153967+nishant-1902@users.noreply.github.com>
2025-08-29 11:41:04 +0530

    Add files via upload

---
+++ IMP Questions.cpp
@@ -0,0 +1,24 @@
+variables
+data types
+operators
+control statement - descision making - if, else-if,switch.
+                  - loop - for,while,do-while.
+                  
+function - advantages,deafault,parameterised,return
+         - diff : call by value vs call by ref.
+
+strings - string function - substring
+
+array - types of array
+
+structure vs union diff
+
+pointer - advantages,disadvantages - array,string,function etc.
+
+file handling - modes,rewind().
+
+
+c-exam-mcq - 40 marks
+      -practical-40 marks
+	  -oral - 20 marks 		 
+		                   
